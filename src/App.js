964725ee5966e89270e098b10854ba0d265c79b8
ZahraO1 <zahra.masroor6@gmail.com>
2022-06-13 16:22:16 -0400

    created structure of page

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+  let [score, setScore]=useState(0)
+  const createMoleHill=()=>{
+
+  }
+  
+  return (
+    <div className="App">
+      <div className="Mole">
+
+      </div>
+      <div className="EmptySlot">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MoleContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Mole from './Mole'
+
+function MoleContainer(props){
+    return(
+        <div>
+            <h2>Mole Container</h2>
+        </div>
+    )
+}
+
+export default MoleContainer
